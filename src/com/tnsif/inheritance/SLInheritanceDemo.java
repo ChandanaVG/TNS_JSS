7e60de96449b0fed2296517859d80f038ddcf768
chandana <chandana85vg@gmail.com>
2025-07-31 19:09:51 +0530

    Single Inheritance Demo

---
+++ src/com/tnsif/inheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.inheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student("Chandana", "Blore", 4567890, 45678909876L, 123, "JSS");
+		System.out.println(s1);
+	}
+
+}
