a3cef2f6f3bb37740414c7f7242271aedd178742
chandana <chandana85vg@gmail.com>
2025-08-12 18:55:39 +0530

    Runtime polymorphism demo.

---
+++ src/com/tnsif/polymorphism/runtime/HDFC.java
@@ -0,0 +1,11 @@
+package com.tnsif.polymorphism.runtime;
+
+public class HDFC extends RBI{
+
+	@Override
+	public float getRateOfInterest() {
+		return 7.0f;
+	}
+	
+	
+}
---
+++ src/com/tnsif/polymorphism/runtime/RBI.java
@@ -0,0 +1,11 @@
+package com.tnsif.polymorphism.runtime;
+
+public class RBI {
+	
+	private final static float ROI=6.4f;
+
+	public float getRateOfInterest() {
+		return ROI;
+	}
+
+} 
