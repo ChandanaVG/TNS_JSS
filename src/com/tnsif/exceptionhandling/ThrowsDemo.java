f934594b5206bc576f368b48fcd9374819895dd7
chandana <chandana85vg@gmail.com>
2025-08-20 18:50:53 +0530

    User-defined exceptions demo.

--- src/com/tnsif/exceptionhandling/ThrowsDemo.java
+++ src/com/tnsif/exceptionhandling/ThrowsDemo.java
@@ -16,6 +16,16 @@ public class ThrowsDemo {
 		s2.setPerc(-1);
 		s2.show();
 		
+		VoterInfo v1 = new VoterInfo();
+		v1.setName("Rahul");
+		v1.setAge(11);
+		System.out.println(v1);
+		
+		VoterInfo v2 = new VoterInfo();
+		v2.setName("Chandana");
+		v2.setAge(20);
+		System.out.println(v2);
+		
 	}
 
 }
