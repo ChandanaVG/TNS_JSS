0588321a573fc536b4669ea1b07cfd6a13bb989e
chandana <chandana85vg@gmail.com>
2025-09-02 18:53:22 +0530

    Upperbounded wildcard demo.

---
+++ src/com/tnsif/generics/UpperBoundedWildCardDemo.java
@@ -0,0 +1,32 @@
+package com.tnsif.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UpperBoundedWildCardDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		intList.add(11);
+		intList.add(22);
+		intList.add(33);
+		System.out.println(UpperBoundedWildCard.sumOfList(intList));
+		
+		ArrayList<Float> floatList = new ArrayList<Float>();
+		floatList.add(11.22f);
+		floatList.add(22.22f);
+		floatList.add(33.3f);
+		System.out.println(UpperBoundedWildCard.sumOfList(floatList));
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		stringList.add("hi");
+		stringList.add("hello");
+		stringList.add("hru");
+		//Can't call methods
+		System.out.println(UpperBoundedWildCard.sumOfList(stringList));
+		
+	}
+
+}
